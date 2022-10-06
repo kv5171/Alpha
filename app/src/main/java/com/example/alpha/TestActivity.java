58f1e47f72b789754b1d7dc513725eba3069de72
keren <kv5171@bs.amalnet.k12.il>
2022-10-06 13:42:54 +0300

    start test

--- app/src/main/java/com/example/alpha/TestActivity.java
+++ app/src/main/java/com/example/alpha/TestActivity.java
@@ -3,18 +3,29 @@ package com.example.alpha;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+
+import java.io.File;
+import java.io.IOException;
 
 public class TestActivity extends AppCompatActivity {
     TextView question;
-    Button answerA, answerB, answerC, answerD;
+    Button answerA, answerB, answerC, answerD, selectedButton;
+    ImageView questionImage;
     Question q;
 
     @Override
@@ -27,6 +38,7 @@ public class TestActivity extends AppCompatActivity {
         answerB = (Button) findViewById(R.id.answerB);
         answerC = (Button) findViewById(R.id.answerC);
         answerD = (Button) findViewById(R.id.answerD);
+        questionImage = (ImageView) findViewById(R.id.questionImage);
 
         getQuestions();
     }
@@ -37,12 +49,34 @@ public class TestActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dS) {
                 q = dS.getValue(Question.class);
-                initUI();
+
+                // if this question has an image - download it!
+                if (!q.getPicturePath().equals(""))
+                {
+                    downloadQuestionPicture(q.getPicturePath());
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
 
+    private void downloadQuestionPicture(String imagePath)
+    {
+        File localFile = new File(getCacheDir().getAbsolutePath() + imagePath);
+
+        FBref.storageRef.child("questions/" + imagePath).getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                // Local temp file has been created
+                initUI();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
             }
         });
     }
@@ -54,5 +88,64 @@ public class TestActivity extends AppCompatActivity {
         answerB.setText(q.getAnswerB());
         answerC.setText(q.getAnswerC());
         answerD.setText(q.getAnswerD());
+
+        if (q.getPicturePath().equals(""))
+        {
+            questionImage.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            questionImage.setVisibility(View.VISIBLE);
+            questionImage.setImageURI(Uri.fromFile(new File(getCacheDir() + q.getPicturePath())));
+        }
+    }
+
+    public void changeSelectedAnswer(View view) {
+        // if there was an answer that was clicked before
+        if (selectedButton != null)
+        {
+            selectedButton.setBackgroundColor(Color.BLUE);
+        }
+
+        selectedButton = (Button) view;
+        view.setBackgroundColor(Color.MAGENTA);
+    }
+
+    public void checkAnswer(View view) {
+        // if no answer was selected
+        if (selectedButton == null)
+        {
+            Toast.makeText(this, "נא לבחור תשובה קודם", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            if (Integer.parseInt(selectedButton.getTag().toString()) == q.getCorrectAnswer())
+            {
+                Toast.makeText(this, "כל הכבוד (נכון)", Toast.LENGTH_SHORT).show();
+                selectedButton.setBackgroundColor(Color.GREEN);
+            }
+            else
+            {
+                Toast.makeText(this, "איזו טעות :(", Toast.LENGTH_SHORT).show();
+                selectedButton.setBackgroundColor(Color.RED);
+
+                // color green the button with the right answer
+                switch (q.getCorrectAnswer())
+                {
+                    case 0:
+                        answerA.setBackgroundColor(Color.GREEN);
+                        break;
+                    case 1:
+                        answerB.setBackgroundColor(Color.GREEN);
+                        break;
+                    case 2:
+                        answerC.setBackgroundColor(Color.GREEN);
+                        break;
+                    case 3:
+                        answerD.setBackgroundColor(Color.GREEN);
+                        break;
+                }
+            }
+        }
     }
 }
